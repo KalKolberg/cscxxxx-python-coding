709e28ea5d8b002bae67875a90bc2e6f6bb937f7
Kal Kolberg <lando2@gmail.com>
2011-07-15 11:27:13 -0400

    Framework for coin calculation program, continued work in class to design this program.

---
+++ coinCalculation.py
@@ -0,0 +1,13 @@
+# Filename:      coinCalculation.py
+# Author:        Kal Kolberg(lando2@gmail.com)
+# Copywrite:     GPL 2.0
+# Description:   Program figures out the maximum amount possible to make without
+#                being able to make change for one dollar
+# Input:         <none>
+# Output:        Maximum dollar amount of coins
+#                Count of each coin type
+#                Number of instructions to find final result
+#                Runtime of program
+# Time to write: 40 minutes 7/14/2011
+# Collaborators: Classroom students
+# Sources:       <none>
